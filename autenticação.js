f6d0e65789ca3953aa6a220326ca87e7db33ac77
miguelourenco <miguelorencodesouza@gmail.com>
2024-06-06 19:39:12 -0300

    remoção da autenticação

--- autenticação.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-    const isAuthenticated = localStorage.getItem('authenticated');
-    if (!isAuthenticated) {
-        alert('Você precisa fazer login primeiro!');
-        window.location.href = 'login.html';
-    }
-});
